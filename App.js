abe2fb8b35cf5d21ffa7224d09caea06ffb55083
ZXCursed <Therealmiyutreaz@gmail.com>
2022-03-10 20:15:36 +0300

    init project

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'
+import LoginScreen from './public/pages/authenfication/Authenfication'
+import Home from './public/pages/home/Home'
+import { COLORS } from './public/templates/style/colors'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Home />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: COLORS().background
   },
-});
+})
